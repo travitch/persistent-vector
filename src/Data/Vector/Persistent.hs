eec0d67cd670ca2de363d91b979e77c318e62cfd
Tristan Ravitch <travitch@cs.wisc.edu>
2012-08-25 14:41:35 -0500

    Switch from vector to arrays for internal storage
    Vector is very sophisticated but this package doesn't need anything
    it provides, and the stream/unstream wrappers do add overhead.
    
    Switch to the Array code from unordered-containers (stolen verbatim).

--- src/Data/Vector/Persistent.hs
+++ src/Data/Vector/Persistent.hs
@@ -1,6 +1,10 @@
 -- | This is a port of the persistent vector from clojure to Haskell.
 --
 -- The implementation is based on array mapped tries.
+--
+-- TODO:
+--
+-- * Implement pop (remove last element)
 module Data.Vector.Persistent (
   Vector,
   empty,
@@ -14,14 +18,10 @@ module Data.Vector.Persistent (
   fromList
   ) where
 
--- TODO: It would be great to have unboxed versions of this using
--- unboxed vectors.  Obviously there is no need for mutable versions
--- as in Vector.
-
 import Prelude hiding ( null, length, tail )
 
-import Control.Applicative ( Applicative, (<$>), (<*>) )
-import qualified Control.Applicative as A
+import Control.Applicative hiding ( empty )
+import Control.DeepSeq
 import Data.Bits
 import Data.Foldable ( Foldable )
 import qualified Data.Foldable as F
@@ -29,19 +29,21 @@ import Data.Monoid ( Monoid )
 import qualified Data.Monoid as M
 import Data.Traversable ( Traversable )
 import qualified Data.Traversable as T
-import qualified Data.Vector as V
+
+import Data.Vector.Persistent.Array ( Array )
+import qualified Data.Vector.Persistent.Array as A
 
 -- Note: using Int here doesn't give the full range of 32 bits on a 32
 -- bit machine (it is fine on 64)
 data Vector a = EmptyVector
               | RootNode { vecSize :: !Int
                          , vecShift :: !Int
-                         , vecTail :: !(V.Vector a)
-                         , intVecPtrs :: !(V.Vector (Vector a))
+                         , vecTail :: !(Array a)
+                         , intVecPtrs :: !(Array (Vector a))
                          }
-              | InternalNode { intVecPtrs :: !(V.Vector (Vector a))
+              | InternalNode { intVecPtrs :: !(Array (Vector a))
                              }
-              | DataNode { dataVec :: !(V.Vector a)
+              | DataNode { dataVec :: !(Array a)
                          }
               deriving (Eq, Ord, Show)
 
@@ -58,16 +60,19 @@ instance Monoid (Vector a) where
 instance Traversable Vector where
   traverse = pvTraverse
 
+instance (NFData a) => NFData (Vector a) where
+  rnf = pvRnf
+
 {-# INLINABLE pvFmap #-}
 pvFmap :: (a -> b) -> Vector a -> Vector b
 pvFmap f = go
   where
     go EmptyVector = EmptyVector
-    go (DataNode v) = DataNode (fmap f v)
-    go (InternalNode v) = InternalNode (fmap (fmap f) v)
+    go (DataNode v) = DataNode (A.map f v)
+    go (InternalNode v) = InternalNode (A.map (fmap f) v)
     go (RootNode sz sh t v) =
-      let t' = fmap f t
-          v' = fmap (fmap f) v
+      let t' = A.map f t
+          v' = A.map (fmap f) v
       in RootNode sz sh t' v'
 
 {-# INLINABLE pvFoldr #-}
@@ -75,28 +80,37 @@ pvFoldr :: (a -> b -> b) -> b -> Vector a -> b
 pvFoldr f = go
   where
     go seed EmptyVector = seed
-    go seed (DataNode v) = V.foldr f seed v
-    go seed (InternalNode vecs) =
-      V.foldr (flip go) seed vecs
-    go seed (RootNode _ _ t vecs) =
-      let tseed = V.foldr f seed t
-      in V.foldr (flip go) tseed vecs
+    go seed (DataNode a) = {-# SCC "goDataNode" #-} A.foldr f seed a
+    go seed (InternalNode as) = {-# SCC "goInternalNode" #-}
+      A.foldr (flip go) seed as
+    go seed (RootNode _ _ t as) = {-# SCC "goRootNode" #-}
+      let tseed = A.foldr f seed t
+      in A.foldr (flip go) tseed as
 
 {-# INLINABLE pvTraverse #-}
 pvTraverse :: (Applicative f) => (a -> f b) -> Vector a -> f (Vector b)
 pvTraverse f = go
   where
-    go EmptyVector = A.pure EmptyVector
-    go (DataNode v) = DataNode <$> T.traverse f v
-    go (InternalNode vecs) = InternalNode <$> T.traverse go vecs
-    go (RootNode sz sh t vecs) =
-      RootNode sz sh <$> T.traverse f t <*> T.traverse go vecs
+    go EmptyVector = pure EmptyVector
+    go (DataNode a) = DataNode <$> A.traverse f a
+    go (InternalNode as) = InternalNode <$> A.traverse go as
+    go (RootNode sz sh t as) =
+      RootNode sz sh <$> A.traverse f t <*> A.traverse go as
 
 {-# INLINABLE pvAppend #-}
 pvAppend :: Vector a -> Vector a -> Vector a
 pvAppend EmptyVector v = v
 pvAppend v EmptyVector = v
 pvAppend v1 v2 = F.foldl' snoc v1 (F.toList v2)
+
+{-# INLINABLE pvRnf #-}
+pvRnf :: (NFData a) => Vector a -> ()
+pvRnf EmptyVector = ()
+pvRnf (DataNode a) = rnf a
+pvRnf (InternalNode a) = rnf a
+pvRnf (RootNode _ _ t as) = rnf as `seq` rnf t
+
+-- Functions
 
 empty :: Vector a
 empty = EmptyVector
@@ -119,65 +133,74 @@ index v ix
 unsafeIndex :: Vector a -> Int -> a
 unsafeIndex vec ix
   | ix >= tailOffset vec =
-    V.unsafeIndex (vecTail vec) (ix .&. 0x1f)
+    A.index (vecTail vec) (ix .&. 0x1f)
   | otherwise = go (fromIntegral (vecShift vec)) vec
   where
     wordIx = fromIntegral ix
     go level v
-      | level == 0 = V.unsafeIndex (dataVec v) (wordIx .&. 0x1f)
+      | level == 0 = A.index (dataVec v) (wordIx .&. 0x1f)
       | otherwise =
         let nextVecIx = (wordIx `shiftR` level) .&. 0x1f
             v' = intVecPtrs v
-        in go (level - 5) (V.unsafeIndex v' nextVecIx)
+        in go (level - 5) (A.index v' nextVecIx)
 
 singleton :: a -> Vector a
 singleton elt =
   RootNode { vecSize = 1
            , vecShift = 5
-           , vecTail = V.singleton elt
-           , intVecPtrs = V.empty
+           , vecTail = A.fromList 1 [elt]
+           , intVecPtrs = A.fromList 0 []
            }
+
+arraySnoc :: Array a -> a -> Array a
+arraySnoc a elt = A.run $ do
+  let alen = A.length a
+  a' <- A.new_ (1 + alen)
+  A.copy a 0 a' 0 alen
+  A.write a' alen elt
+  return a'
 
 snoc :: Vector a -> a -> Vector a
 snoc EmptyVector elt = singleton elt
 snoc v@RootNode { vecSize = sz, vecShift = sh, vecTail = t } elt
   -- Room in tail
-  | not (nodeIsFull t) = v { vecTail = V.snoc t elt, vecSize = sz + 1 }
+  | not (nodeIsFull t) = v { vecTail = arraySnoc t elt, vecSize = sz + 1 }
   -- Overflow current root
   | sz `shiftR` 5 > 1 `shiftL` sh =
     RootNode { vecSize = sz + 1
              , vecShift = sh + 5
-             , vecTail = V.singleton elt
-             , intVecPtrs = V.fromList [ InternalNode (intVecPtrs v)
-                                       , newPath sh t
-                                       ]
+             , vecTail = A.fromList 1 [elt]
+             , intVecPtrs = A.fromList 2 [ InternalNode (intVecPtrs v)
+                                         , newPath sh t
+                                         ]
              }
   -- Insert into the tree
   | otherwise =
       RootNode { vecSize = sz + 1
                , vecShift = sh
-               , vecTail = V.singleton elt
+               , vecTail = A.fromList 1 [elt]
                , intVecPtrs = pushTail sz t sh (intVecPtrs v)
                }
 snoc _ _ = error "Internal nodes should not be exposed to the user"
 
-pushTail :: Int -> V.Vector a -> Int -> V.Vector (Vector a) -> V.Vector (Vector a)
+pushTail :: Int -> Array a -> Int -> Array (Vector a) -> Array (Vector a)
 pushTail cnt t = go
   where
     go level parent
-      | level == 5 = V.snoc parent (DataNode t)
-      | subIdx < V.length parent =
-        let nextVec = V.unsafeIndex parent subIdx
+      | level == 5 = arraySnoc parent (DataNode t)
+      | subIdx < A.length parent =
+        let nextVec = A.index parent subIdx
             toInsert = go (level - 5) (intVecPtrs nextVec)
-        in parent V.// [(subIdx, InternalNode toInsert)]
-      | otherwise = V.snoc parent (newPath (level - 5) t)
+        in A.update parent subIdx (InternalNode toInsert)
+--         parent V.// [(subIdx, InternalNode toInsert)]
+      | otherwise = arraySnoc parent (newPath (level - 5) t)
       where
         subIdx = ((cnt - 1) `shiftR` level) .&. 0x1f
 
-newPath :: Int -> V.Vector a -> Vector a
+newPath :: Int -> Array a -> Vector a
 newPath level t
   | level == 0 = DataNode t
-  | otherwise = InternalNode (V.singleton (newPath (level - 5) t))
+  | otherwise = InternalNode $ A.fromList 1 $ [newPath (level - 5) t]
 
 
 tailOffset :: Vector a -> Int
@@ -187,8 +210,8 @@ tailOffset v
   where
     len = length v
 
-nodeIsFull :: V.Vector a -> Bool
-nodeIsFull = (==32) . V.length
+nodeIsFull :: Array a -> Bool
+nodeIsFull = (==32) . A.length
 
 fromList :: [a] -> Vector a
 fromList = F.foldl' snoc empty
